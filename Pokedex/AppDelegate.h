2d91870f0954a832fa3c162281f9249c1b224554
Nelson Bolívar <nelsonb21@gmail.com>
2016-09-17 09:16:36 -0500

    Initial Commit

---
+++ Pokedex/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pokedex
+//
+//  Created by Nelson Bolivar on 9/17/16.
+//  Copyright © 2016 Nelson Bolivar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
